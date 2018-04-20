ed00e0339b410797e605e7d01b05ba7cdb7af318
Håvard Svoen <svoenhav@visma.com>
2018-04-20 16:00:58 +0200

    Add project files.

---
+++ GetEmailSlackBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlackAPI;
+
+namespace GetEmailSlackBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
