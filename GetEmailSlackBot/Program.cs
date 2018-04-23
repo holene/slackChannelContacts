16b59eb72bcc392c0b4c1126f4609ce3cc7d45cd
Håvard Svoen <svoenhav@visma.com>
2018-04-23 09:27:00 +0200

    removed token from source control

--- GetEmailSlackBot/Program.cs
+++ GetEmailSlackBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,10 @@ namespace GetEmailSlackBot
     {
         static void Main(string[] args)
         {
+
+            string BotToken = ConfigurationManager.AppSettings["bottoken"];
+
+            Console.WriteLine(BotToken);
         }
     }
 }
